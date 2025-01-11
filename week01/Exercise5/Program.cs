7aa350f01769bfd5efba9eb53b30eaf41d1f2172
sharonramirez21 <sharonramirez421@gmail.com>
2025-01-10 23:15:19 -0300

    Activity #5 ready :)

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // calling the functions
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        
+        DisplayResult(userName, squaredNumber);
     }
+
+    // #step 1
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        // #step 2
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please, enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        // #step3
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        // #step 4
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        // #step 5
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
 }
